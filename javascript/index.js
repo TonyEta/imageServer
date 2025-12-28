8fb75f31f2c2707a805713a56f7acd7bbeecee8d
Max <shamruk01@gmail.com>
2025-12-28 15:16:04 +0300

    create upload page with CSS/JS

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("header_button")
     if (button) {
         button.addEventListener("click", () => {
-            document.location.href = "html/loadingPage.html";
+            document.location.href = "html/UploadPage.html";
         });
     }
 });
